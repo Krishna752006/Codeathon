f4b72e75860e99d9f302310b7d1dae54862b7749
Krishna752006 <kkc070506@gmail.com>
2026-03-29 14:32:06 +0530

    All

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_colwidth', None)
-
-df = pd.read_csv('Linkedin job listings information.csv')
-print(df.shape)
-print(df.columns)
